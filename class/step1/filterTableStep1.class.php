50d551113169228a04e170d8ade5a33acd3afe76
Samuel Melrose <sam.melrose@a1comms.co.uk>
2012-11-05 16:15:37 +0000

    Finished Step1 Filter Table

--- class/step1/filterTableStep1.class.php
+++ class/step1/filterTableStep1.class.php
@@ -4,6 +4,10 @@ class filterTableStep1 extends Step1 {
 	
 	protected function _parse(){
 		$this->_defaultChains();
+		$this->_fwChains();
+		$this->_ifaceChains();
+		$this->_serviceChains();
+		$this->_otherChains();
 	}
 	
 	private function _validDefaultPolicy($policy){
@@ -79,6 +83,130 @@ class filterTableStep1 extends Step1 {
 			}
 		} else {
 			$this->logError( 'Warning: default chains block doesn\'t exist for filter table.', false);
+		}
+	}
+	
+	private function _fwChains(){
+		if ( @is_array( $this->pdata['fw-chains'] ) ){
+			foreach( $this->pdata['fw-chains'] as $fwchain ){
+				if ( @in_array($fwchain['from'], array_keys($this->dataArray['interfaces'])) ){
+					if ( @is_array($this->dataArray['tables']['filter']['fw-chains'][$fwchain['from']]) ){
+						$this->logError( 'Error: Firewall Chain for This Interface Already Exists - Duplicate - ' . var_export($fwchain, true), true);
+					} else {
+						$this->dataArray['tables']['filter']['fw-chains'][$fwchain['from']] = array();
+						if ( @is_array($fwchain['rules']) ){
+							foreach ($fwchain['rules'] as $rule){
+								$this->dataArray['tables']['filter']['fw-chains'][$fwchain['from']]['rules'][] = $rule;
+							}
+						} else {
+							$this->logError( 'Error: Invalid Rules Array for fw-chain - ' . var_export($fwchain, true), true);
+						}
+						if (!empty($fwchain['default-goto'])){
+							$this->dataArray['tables']['filter']['fw-chains']['default-goto'] = $fwchain['default-goto'];
+						} else {
+							$this->logError( 'Warning: Default-Goto for fw-chain not specified - Using default of DROP', false);
+							$this->dataArray['tables']['filter']['fw-chains']['default-goto'] = 'DROP';
+						}
+					}
+				} else {
+					$this->logError( 'Error: invalid interface listed for firewall chain - ' . var_export($fwchain, true), true);
+				}
+			}
+		} else {
+			$this->logError( 'Warning: firewall chains block doesn\'t exist for filter table.', false);
+		}
+	}
+	
+	private function _ifaceChains(){
+		if ( @is_array( $this->pdata['iface-chains'] ) ){
+			foreach( $this->pdata['iface-chains'] as $ifchain ){
+				if ( ( !empty($ifchain['to']) ) && (@in_array($ifchain['to'], array_keys($this->dataArray['interfaces']))) ){
+					if ( ( !empty($ifchain['from']) ) && (@in_array($ifchain['from'], array_keys($this->dataArray['interfaces']))) ){
+						if ( $ifchain['to'] != $ifchain['from'] ){
+							if ( @is_array($ifchain['rules']) ){
+								$chain = array( 'to' => $ifchain['to'], 'from' => $ifchain['from'], 'default-goto' => 'ACCEPT', 'rules' => array() );
+								foreach ( $ifchain['rules'] as $rule ){
+									$chain['rules'][] = $rule;
+								}
+								if ( !empty( $ifchain['default-goto'] ) ){
+									$chain['default-goto'] = $ifchain['default-goto'];
+								} else {
+									$this->logError( 'Warning: using default-goto of ACCEPT for iface-chain as none specified!', false);
+								}
+								$this->dataArray['tables']['filter']['iface-chains'][] = $chain;
+							}
+						} else {
+							$this->logError( 'Error: interface chain can\'t filter traffic from and to the same interface without Proxy-ARP enabled', true);
+						}
+					} else {
+						$this->logError( 'Error: Invalid from Interface on iface-chain - ' . var_export($ifchain, true), true);
+					}
+				} else {
+					$this->logError( 'Error: Invalid to Interface on iface-chain - ' . var_export($ifchain, true), true);
+				}
+			}
+		} else {
+			$this->logError( 'Warning: interface chains block doesn\'t exist for filter table.', false);
+		}
+	}
+	
+	private function _serviceChains(){
+		if ( @is_array( $this->pdata['service-chains'] ) ){
+			foreach( $this->pdata['service-chains'] as $schain ){
+				if ( !empty( $schain['name'] ) ){
+					if ( ! @is_array($this->dataArray['tables']['filter']['service-chains'][$schain['name']]) ){
+						if ( @is_array( $schain['rules'] ) ){
+							$this->dataArray['tables']['filter']['service-chains'][$schain['name']] = array('default-goto' => 'ACCEPT', 'rules' => array());
+							foreach ( $schain['rules'] as $rule ){
+								$this->dataArray['tables']['filter']['service-chains'][$schain['name']]['rules'][] = $rule;
+							}
+							if ( !empty( $schain['default-goto'] ) ){
+								$this->dataArray['tables']['filter']['service-chains'][$schain['name']]['default-goto'] = $schain['default-goto'];
+							} else {
+								$this->logError( 'Warning: using default-goto of ACCEPT for service-chain as none specified!', false);
+							}
+						} else {
+							$this->logError( 'Error: rules array missing from service chain - ' . var_dump($schain, true), true);
+						}
+					} else {
+						$this->logError( 'Error: service chain with this name already exists - ' . var_dump($schain, true), true);
+					}
+				} else {
+					$this->logError( 'Error: you must specify a name for a service chain - ' . var_dump($schain, true), true);
+				}
+			}
+		} else {
+			$this->logError( 'Warning: service chains block doesn\'t exist.', false);
+		}
+	}
+	
+	private function _otherChains(){
+		if ( @is_array( $this->pdata['other-chains'] ) ){
+			foreach( $this->pdata['other-chains'] as $ochain ){
+				if ( !empty( $ochain['name'] ) ){
+					if ( ! @is_array($this->dataArray['tables']['filter']['other-chains'][$ochain['name']]) ){
+						if ( @is_array( $ochain['rules'] ) ){
+							$this->dataArray['tables']['filter']['other-chains'][$ochain['name']] = array('default-goto' => 'ACCEPT', 'rules' => array());
+							foreach ( $ochain['rules'] as $rule ){
+								$this->dataArray['tables']['filter']['other-chains'][$ochain['name']]['rules'][] = $rule;
+							}
+							if ( !empty( $ochain['default-goto'] ) ){
+								$this->dataArray['tables']['filter']['other-chains'][$ochain['name']]['default-goto'] = $ochain['default-goto'];
+							} else {
+								$this->logError( 'Warning: using default-goto of ACCEPT for other-chain as none specified!', false);
+							}
+						} else {
+							$this->logError( 'Error: rules array missing from other chain - ' . var_dump($ochain, true), true);
+						}
+					} else {
+						$this->logError( 'Error: \'other\' chain with this name already exists - ' . var_dump($ochain, true), true);
+					}
+				} else {
+					$this->logError( 'Error: you must specify a name for a other chain - ' . var_dump($ochain, true), true);
+				}
+			}
+		} else {
+			$this->logError( 'Warning: service chains block doesn\'t exist.', false);
 		}
 	}
 	
